08a96db073c155454db8cca88d9d086652d86974
Luxem <luxem192@gmail.com>
2021-12-14 21:34:07 +0100

    WOW

---
+++ Network.gd
@@ -0,0 +1,45 @@
+extends Node
+
+var network = NetworkedMultiplayerENet.new()
+var port:int = 1770
+var maxPlayers:int = 2
+var playerStateCollection = {}
+var playerId = []
+
+func _ready():
+	network.create_server(port, maxPlayers)
+	get_tree().set_network_peer(network)
+	print("server lancé")
+	
+	
+	get_tree().connect("network_peer_connected", self, "_player_connected")
+	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
+
+
+func _player_connected(id):
+	print("Le joueur d'id : ", id, " s'est connecté")
+	playerId.append(id)
+
+func _player_disconnected(id):
+	print("Le joueur d'id : ", id, " s'est deconnecté")
+	playerId.erase(id)
+
+remote func spawnBalloon(type,path,idBalloon):
+	print("on spawn des balloons let's go server :")
+	var id = get_tree().get_rpc_sender_id()
+	for player in playerId:
+		if player!=id:
+			rpc_id(player, "spawnBalloon3D", type,path,idBalloon)
+
+remote func receiveKillBalloon(parentPath,balloonId):
+	var id = get_tree().get_rpc_sender_id()
+	for player in playerId:
+		if player!=id:
+			rpc_id(player, "killBalloon", parentPath,balloonId)
+
+remote func remotePop(balloon):
+	print(balloon)
+	var id = get_tree().get_rpc_sender_id()
+	for player in playerId:
+		if player!=id:
+			rpc_id(player, "receivePop", balloon)
